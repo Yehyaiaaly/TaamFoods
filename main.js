9d003dbfae23954b7de449a4797ef2b8eeae7532
Yehyaiaaly <135841221+Yehyaiaaly@users.noreply.github.com>
2023-06-07 16:04:19 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+ function order() {
+            let p, cp, b, cb, f, h, s, c;
+            q = document.getElementById("pa");
+            window.alert("You have ordered a : " + q.textContent);
+        }
+        function ord() {
+            cp = document.getElementById("cp");
+            window.alert("You have ordered a : " + cp.textContent);
+        }
+
+        function orde() {
+            b = document.getElementById("b");
+            window.alert("You have ordered a : " + b.textContent);
+        }
+        function o1() {
+            cb = document.getElementById("cb");
+            window.alert("You have ordered a : " + cb.textContent);
+        }
+        function o2() {
+            h = document.getElementById("h");
+            window.alert("You have ordered a : " + h.textContent);
+
+        }
+        function o3() {
+            f = document.getElementById("f");
+            window.alert("You have ordered a : " + f.textContent);
+        }
+        function o4() {
+            s = document.getElementById("s");
+            window.alert("You have ordered a : " + s.textContent);
+        }
+
+function o5() {
+    c = document.getElementById("c");
+    window.alert("You have ordered a : " + c.textContent);
+}
